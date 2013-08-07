57bc54fea552e3f804c2d221a4866a8f8d8d6167
Andrew Moist <andyjm@gmail.com>
2013-08-07 18:59:47 +0100

    Updated clean.js script, added blank express app.

--- data/clean.js
+++ data/clean.js
@@ -1,51 +1,39 @@
 var mongoose = require('mongoose');
-	_ = require('underscore');
+	_ = require('underscore'),
+	async = require('async');
 mongoose.connect('mongodb://localhost/landwiki');
 
 var landSchema = mongoose.Schema({
   longitude: Number,
   latitude: Number,
+  title: String,
   labels:   String,
   status: Number,
+  icon: String,
   updated: { type: Date, default: Date.now },
   originalData: mongoose.Schema.Types.Mixed
 });
 var Land = mongoose.model('land', landSchema);
 
-var lastUpdated = 1;
 var i = 1;
 
-// if (!landSchema.options.toObject) landSchema.options.toObject = {};
-// landSchema.options.toObject.transform = function (doc, ret, options) {
-//   var originalData = ret;
-//   delete originalData._id;
-//   return {
-// 	longitude: ret.Longitude,
-// 	latitude: ret.Latitude,
-// 	labels: 'nlud-pdl-2009 hca ukgov brownfield',
-// 	status: 1,
-// 	originalData: originalData
-//   };
-// }
+function calculateIcon(data) {
+	if (data['Planning status'] !== 'None')
+		return 'small_red';
+	return 'measle_brown';
+}
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-  // yay!
-
-  console.log('connection open');
+function batchUpdate() {
 
 	Land
-	.find({ updated:  { $ne: '1' }  })
-	//.find()
-	//.select('_id')
-	.limit(10)
+	.find({ "status":  { "$exists" : false } })
+	.limit(500)
 	.exec(function(err, results) {
 		if (err) {
 			console.log(err);
 			return;
 		}
-		results.forEach(function(doc) {
+		async.forEach(results, function(doc, callback) {
 
 			var originalData = doc.toObject();
 
@@ -55,7 +43,8 @@ db.once('open', function callback () {
 				title: originalData.Title,
 				labels: 'nlud-pdl-2009 hca ukgov brownfield',
 				status: 1,
-				originalData: originalData
+				originalData: originalData,
+				icon: calculateIcon(originalData)
 			};
 			delete landRecord.originalData._id;
 			delete landRecord.originalData.EASTING;
@@ -67,16 +56,40 @@ db.once('open', function callback () {
 			delete landRecord.originalData.updated;
 			delete landRecord.originalData.title;
 
-			console.log(landRecord);
-			var newLand = new Land(landRecord);
+			doc.status = 0;
+			doc.save();
 
+			//console.log(landRecord);
+			var newLand = new Land(landRecord);
 			newLand.save(function(err, newDoc, numberAffected) {
 				if (err) {
 					console.log('Failed to save', err);
-					return;
+					return callback(err);
 				}
 				console.log('%d updated %s, %s records updated', i++, newDoc._id, numberAffected);
+
+				callback(err);
 			});
+		}, function onComplete(err) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			if (results.length > 0)
+				batchUpdate();
+			else 
+				console.log('complete');
 		});
 	});
+}
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  // yay!
+
+	console.log('connection open');
+
+ 	batchUpdate();
+
 });
