ef61da228ccc8c30fa95a462341a8d458f4dbc07
Andrew Moist <andyjm@gmail.com>
2013-10-22 18:22:32 +0200

    Introduced requirejs and backbone. Moved map code into view.

--- public/js/main.js
+++ public/js/main.js
@@ -1,70 +1,33 @@
-
-var map,
-    layer;
-
-function initialize() {
-  google.maps.visualRefresh = true;
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(52.725390548214925, -1.745800962499966),
-    zoom: 7,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(document.getElementById('googft-legend'));
-
-  layer = new google.maps.FusionTablesLayer({
-    map: map,
-    heatmap: { enabled: false },
-    query: getQuery(),
-    options: {
-      styleId: 2,
-      templateId: 2
+require.config({
+  baseUrl: "js/",
+  paths: {
+    backbone: '/components/backbone/backbone-min',
+    underscore: '/components/underscore/underscore-min'
+  },
+  shim: {
+    'backbone': {
+        //These script dependencies should be loaded before loading
+        //backbone.js
+        deps: ['underscore', 'jquery'],
+        //Once loaded, use the global 'Backbone' as the
+        //module value.
+        exports: 'Backbone'
+    },
+    'underscore': {
+        exports: '_'
     }
-  });
-
-  var input = /** @type {HTMLInputElement} */(document.getElementById('search'));
-  var searchBox = new google.maps.places.SearchBox(input);
-  var markers = [];
-
-  google.maps.event.addListener(searchBox, 'places_changed', function() {
-    var places = searchBox.getPlaces();
-    map.setCenter(places[0].geometry.location);
-    map.setZoom(14);
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-$(function() {
-  var $sizeFilterOptions = $('#filters input');
-  $sizeFilterOptions.on('change', function() {
-    
-    var conditions = [];
-    $sizeFilterOptions.each(function() {
-      if (! this.checked)
-        return;
-      conditions.push($(this).data('query'));
-    });
-    var applyFilter = conditions.length > 0 && conditions.length !== $sizeFilterOptions.length;
-    var whereClause = applyFilter ? conditions.join(' OR ') : '';
-    console.log('query', whereClause);
-
-    layer.setQuery(getQuery(whereClause));
-  });
-
-  // $('.googft-info-window a').on('click', function() { 
-  //   console.log('open');
-  //   return false;
-  // });
-
-  // Bootstrap
-  $('#filters label').tooltip();
+  }
 
 });
 
-function getQuery(whereClause) {
-  return {
-    select: "col1",
-    from: "1i3YjHvkMCfoc3HMbEWWec1W3gdtOZpJZf4_P87o",
-    where: whereClause || ''
-  };
-}
+require(["jquery", "views/map"], function($, MapView, MapSearchView) {
+  $(function() {
+
+      var mapView = new MapView({ el: $('#map') });
+      mapView.bindEvents();
+
+      // var mapSearchView = new MapSearchView({ el: $('') });
+      // mapView.bindEvents();
+
+  });
+});
---
+++ public/js/views/map.js
@@ -0,0 +1,95 @@
+define([
+  'backbone',
+  'underscore'
+  ], function(Backbone, _) {
+
+  var MapView = Backbone.View.extend({
+
+    initialize: function() {
+      this.map = null;
+      this.layer = null;
+      this.searchBox = null;
+      this.$sizeFilterOptions = $('#filters input');
+
+      this.initMap();
+    },
+
+    initMap: function() {
+      google.maps.visualRefresh = true;
+      this.map = new google.maps.Map(this.el, {
+        center: new google.maps.LatLng(52.725390548214925, -1.745800962499966),
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+      this.map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(document.getElementById('googft-legend'));
+
+      this.layer = new google.maps.FusionTablesLayer({
+        map: this.map,
+        heatmap: { enabled: false },
+        query: this.getQuery(),
+        options: {
+          styleId: 2,
+          templateId: 2
+        }
+      });
+
+    },
+
+    bindEvents: function() {
+      var input = document.getElementById('search');
+      this.searchBox = new google.maps.places.SearchBox(input);
+      google.maps.event.addListener(this.searchBox, 'places_changed', _.bind(this.zoomToPlace, this));
+
+      this.$sizeFilterOptions.on('change', _.bind(this.updateMapQuery, this));
+
+      var self = this;
+      this.$el.on('click', '.googft-info-window a', function() {
+        self.handleLandDetailLink(this);
+        return false;
+      });
+    },
+
+    handleLandDetailLink: function(link) {
+      // Unfortunately Google does not allow us to use data attributes in the template
+      // So we must pluck the ID out manually.
+      var id = link.href.substr(link.href.lastIndexOf('/')+1, link.href.length);
+      console.log('land id', id);
+    },
+
+    updateMapQuery: function() {
+      var conditions = [];
+      this.$sizeFilterOptions.each(function() {
+        if (! this.checked)
+          return;
+        conditions.push($(this).data('query'));
+      });
+      var applyFilter = conditions.length > 0 && conditions.length !== this.$sizeFilterOptions.length;
+      var whereClause = applyFilter ? conditions.join(' OR ') : '';
+      console.log('query', whereClause);
+
+      this.layer.setQuery(this.getQuery(whereClause));
+    },
+
+    zoomToPlace: function() {
+      var places = this.searchBox.getPlaces();
+      this.map.setCenter(places[0].geometry.location);
+      this.map.setZoom(14);
+    },
+
+    getQuery: function (whereClause) {
+      return {
+        select: "col1",
+        from: "1i3YjHvkMCfoc3HMbEWWec1W3gdtOZpJZf4_P87o",
+        where: whereClause || ''
+      };
+    },
+
+    render: function() {
+      // content is pre-prendered on index page
+      return this;
+    }
+
+  });
+
+  return MapView;
+});
